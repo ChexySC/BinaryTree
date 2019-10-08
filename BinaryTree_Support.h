9a7556bc1fc6da85e225bd91949c1f99fbca7f2e
ChexySC <sheldon_xi@outlook.com>
2019-10-08 10:35:36 +0800

    Create BinaryTree_Support.h

---
+++ BinaryTree_Support.h
@@ -0,0 +1,9 @@
+#ifndef BINARY_TREE_SUPPORT_H
+#define BINARY_TREE_SUPPORT_H
+#include "BinaryTree.h"
+void InitNode_Full(_Node* node);
+void Out(_Node* node);
+void ShowData(_Node* node);
+int keyCompare(int key, int data);
+
+#endif
