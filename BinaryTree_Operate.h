1e040b10228419c46d24a7f6338602b8f1cd47fb
ChexySC <sheldon_xi@outlook.com>
2019-10-08 10:34:40 +0800

    Create BinaryTree_Operate.h

---
+++ BinaryTree_Operate.h
@@ -0,0 +1,9 @@
+#ifndef BINARY_TREE_OPERATE_H
+#define BINARY_TREE_OPERATE_H
+
+#include "BinaryTree.h"
+void InitSearchData(_Node* node);
+
+void BinartTree_Search_Main(_Node* searchTree);
+void BinartTree_Insert_Main();
+#endif
