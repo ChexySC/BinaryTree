99070cf82f4169c7774cd264aaf3c5f2b26bbb6c
ChexySC <sheldon_xi@outlook.com>
2019-10-08 10:36:27 +0800

    Create BinaryTree_Traversal.h

---
+++ BinaryTree_Traversal.h
@@ -0,0 +1,10 @@
+#ifndef BINARY_TREE_TRAVERSAL_H
+#define BINARY_TREE_TRAVERSAL_H
+#include "BinaryTree.h"
+void Forward_Traversal(_Node* node);
+void Mid_Traversal(_Node* node);
+void Back_Traversal(_Node* node);
+void InitTraversalData(_Node* node);
+
+void BinartTree_Traversal(_Node* treeNode);
+#endif
